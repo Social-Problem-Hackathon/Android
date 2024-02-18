eeee79152cb03058759e6cd0e40ac4fb405a03c6
ss99x2002 <ss99x200211@gmail.com>
2024-02-18 19:42:42 +0900

    [Add/#3] 채팅 fragment 클래스 추가

---
+++ app/src/main/java/com/example/socialproblemandroid/presentation/ChatFragment.kt
@@ -0,0 +1,9 @@
+package com.example.socialproblemandroid.presentation
+
+import com.example.socialproblemandroid.R
+import com.example.socialproblemandroid.databinding.FragmentChatBinding
+import com.example.socialproblemandroid.util.BindingFragment
+
+class ChatFragment : BindingFragment<FragmentChatBinding> (R.layout.fragment_chat) {
+
+}
