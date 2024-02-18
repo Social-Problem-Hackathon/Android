2879a963677bc0672e4a4abc5e67750dc82ab367
ss99x2002 <ss99x200211@gmail.com>
2024-02-18 19:45:10 +0900

    [Add/#3] 홈 Fragment 추가

---
+++ app/src/main/java/com/example/socialproblemandroid/presentation/HomeFragment.kt
@@ -0,0 +1,9 @@
+package com.example.socialproblemandroid.presentation
+
+import com.example.socialproblemandroid.R
+import com.example.socialproblemandroid.databinding.FragmentHomeBinding
+import com.example.socialproblemandroid.util.BindingFragment
+
+class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home){
+
+}
