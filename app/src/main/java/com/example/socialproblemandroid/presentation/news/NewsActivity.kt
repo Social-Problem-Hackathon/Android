f3f0af896975bb616ff2f75071ce509d75698b0b
ss99x2002 <ss99x200211@gmail.com>
2024-02-26 02:11:31 +0900

    [Add/#5] NewsActivity 생성

---
+++ app/src/main/java/com/example/socialproblemandroid/presentation/news/NewsActivity.kt
@@ -0,0 +1,14 @@
+package com.example.socialproblemandroid.presentation.news
+
+import android.os.Bundle
+import com.example.socialproblemandroid.R
+import com.example.socialproblemandroid.databinding.ActivityNewsBinding
+import com.example.socialproblemandroid.util.base.BindingActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class NewsActivity : BindingActivity<ActivityNewsBinding>(R.layout.activity_news) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
