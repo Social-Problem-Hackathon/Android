62945db801a47468110a052e9ad1aaffe6bc5ffb
ss99x2002 <ss99x200211@gmail.com>
2024-02-26 03:32:18 +0900

    [Feat/#7] 관계부처에 따른 뉴스 태그 분류 기능 개발

--- app/src/main/java/com/example/socialproblemandroid/presentation/home/news/NewsAdapter.kt
+++ app/src/main/java/com/example/socialproblemandroid/presentation/home/news/NewsAdapter.kt
@@ -6,15 +6,18 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
+import coil.transform.CircleCropTransformation
+import com.example.socialproblemandroid.R
 import com.example.socialproblemandroid.data.model.remote.response.NewsItem
 import com.example.socialproblemandroid.data.model.remote.response.NewsResponse
 import com.example.socialproblemandroid.databinding.ItemTodayNewsBinding
 import com.example.socialproblemandroid.util.setOnSingleClickListener
 
 
-class NewsAdapter(private val itemClick: (NewsItem) -> (Unit)) : ListAdapter<NewsItem, NewsAdapter.NewsViewHolder>(diffUtil) {
+class NewsAdapter(private val itemClick: (NewsItem) -> (Unit)) :
+    ListAdapter<NewsItem, NewsAdapter.NewsViewHolder>(diffUtil) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder{
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
         val binding =
             ItemTodayNewsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return NewsViewHolder(binding, itemClick)
@@ -28,13 +31,34 @@ class NewsAdapter(private val itemClick: (NewsItem) -> (Unit)) : ListAdapter<New
         private val binding: ItemTodayNewsBinding,
         private val itemClick: (NewsItem) -> (Unit)
     ) : RecyclerView.ViewHolder(binding.root) {
+        private fun categorizeTag(ministerCode: String): String {
+            return when {
+                KEYWORDS_ECONOMY.any { it in ministerCode } -> "#경제 #돈 #관세 #수입 #수출"
+                KEYWORDS_LABOR.any { it in ministerCode } -> "#노동 #고용"
+                KEYWORDS_SOCIETY.any { it in ministerCode } -> "#사회 #외교"
+                KEYWORDS_WELFARE.any { it in ministerCode } -> "#의료 #복지"
+                else -> ""
+            }
+        }
+
         fun onBind(data: NewsItem) {
             with(binding) {
                 tvNewsTopic.text = data.title
-                ivNewsTumbnail.load(data.originalImgUrl){
+                ivNewsTumbnail.load(data.originalImgUrl) {
                     crossfade(true)
                 }
-                tvNewsTag.text = "" //content에 contains로 tag 판단 (type 사용하기!)
+                tvNewsTag.text = categorizeTag(data.ministerCode!!)
+                ivTopicTumbnail.load(
+                    when (categorizeTag(data.ministerCode)) {
+                        "#경제 #돈 #관세 #수입 #수출" -> R.drawable.img_money
+                        "#노동 #고용" -> R.drawable.img_labor
+                        "#사회 #외교" -> R.drawable.img_society
+                        "#의료 #복지" -> R.drawable.img_welfare
+                        else -> R.drawable.img_white
+                    }
+                ) {
+                    transformations(CircleCropTransformation())
+                }
                 root.setOnSingleClickListener {
                     itemClick(data)
                 }
@@ -42,8 +66,12 @@ class NewsAdapter(private val itemClick: (NewsItem) -> (Unit)) : ListAdapter<New
         }
     }
 
-
     companion object {
+        val KEYWORDS_ECONOMY = listOf("기획재정부", "농림축산식품부", "산업통상자원부" ,"해양수산부", "중소벤처기업부")
+        val KEYWORDS_LABOR = listOf("고용노동부")
+        val KEYWORDS_WELFARE = listOf("보건복지부", "여성가족부", "방송통신위원회", "행정안전부", "소방청")
+        val KEYWORDS_SOCIETY = listOf("환경부", "행정자치부", "외교부", "문화체육관광부", "교육부", "통일부")
+
         val diffUtil = object : DiffUtil.ItemCallback<NewsItem>() {
             override fun areItemsTheSame(
                 oldItem: NewsItem,
@@ -60,4 +88,5 @@ class NewsAdapter(private val itemClick: (NewsItem) -> (Unit)) : ListAdapter<New
             }
         }
     }
+
 }
