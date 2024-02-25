693c723bcda669e91e518d02195ae93f542cc5d4
ss99x2002 <ss99x200211@gmail.com>
2024-02-26 02:08:37 +0900

    [Add/#5] MyPageFragment @AndroidEntryPoint 어노테이션 추가

--- app/src/main/java/com/example/socialproblemandroid/presentation/MyPageFragment.kt
+++ app/src/main/java/com/example/socialproblemandroid/presentation/MyPageFragment.kt
@@ -2,7 +2,9 @@ package com.example.socialproblemandroid.presentation
 
 import com.example.socialproblemandroid.R
 import com.example.socialproblemandroid.databinding.FragmentMypageBinding
-import com.example.socialproblemandroid.util.BindingFragment
+import com.example.socialproblemandroid.util.base.BindingFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MyPageFragment : BindingFragment<FragmentMypageBinding>(R.layout.fragment_mypage) {
 }
